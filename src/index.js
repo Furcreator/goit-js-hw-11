831b4c9f132fd75bc64a7c1c526a557bbb45b903
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-03-31 20:13:30 +0300

    4

--- src/index.js
+++ src/index.js
@@ -3,40 +3,56 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { search } from './search';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
 var lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionPosition: "bottom",
-    captionDelay: 250,
+    // captionsData: "alt",
+    // captionPosition: "bottom",
+    // captionDelay: 250,
 });
 
 
 const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
 
+window.addEventListener('scroll', debounce(onScrollDocument, DEBOUNCE_DELAY));
 form.addEventListener('submit', searchImg);
+
+let page = 1;
+let request = '';
+let arrayImg = '';
+let hits = '';
 
 async function searchImg(e) {
     e.preventDefault();
     gallery.innerHTML = '';
     const {elements: { searchQuery }} = e.currentTarget;
-    let request = searchQuery.value.trim();
-    const response = await search(request)
-    const arrayImg = response.data.hits;
-    const hits = response.data.totalHits
+    request = searchQuery.value.trim();
+    form.reset();
+
     if(request === ''){
         gallery.innerHTML = '';
         return;
     }
+    const response = await search(request, page)
+    arrayImg = response.data.hits;
+    hits = response.data.totalHits
+    console.log(arrayImg.length);
+
+    if(arrayImg.length <= 0){
+     errorMessage();
+     return;
+    }
+    
     renderItemList(arrayImg)
     hitsMessages(hits);
-    form.reset()
+  
 }
 
 function renderItemList(item) {
-    listItem = item.map(item =>
+   const listItem = item.map(item =>
         `<div class="photo-card">
                   <a href="${item.largeImageURL}">      
                   <div class="thumb">  
@@ -58,8 +74,25 @@ function renderItemList(item) {
         .join('');
 
     gallery.insertAdjacentHTML('beforeend', listItem);
+    lightbox.refresh();
 }
 
 function hitsMessages(hits) {
     Notify.success(`Hooray! We found ${hits} images.`, {timeout: 5000})
 }
+
+function onScrollDocument(){
+    const scroll = document.documentElement.getBoundingClientRect()
+    if(scroll.bottom < document.documentElement.clientHeight + 150){
+        page ++;
+        search(request, page);
+        renderItemList(arrayImg);
+    }
+}
+
+function errorMessage() {
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.', {timeout: 3000})
+}
+
+
+//! функции по: 1(сообщение после прокрутки в самый низ галереии) 2(сообщение при пустом инпуте) 3(симпл лайт бокс)
--- src/search.js
+++ src/search.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 const url = 'https://pixabay.com/api/'
 const key = '34923936-72b1522875746ba4d44cc2019'
-let page = 1;
 
-async function search(item) {
+async function search(item, page) {
     const adress = `${url}?key=${key}&q=${item}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     try {
         return await axios.get(adress);
